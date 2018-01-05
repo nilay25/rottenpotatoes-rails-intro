94587d7987d70179eb9cc07c4029768647ac6f0c
Nilay Patel <nilay25@tamu.edu>
2018-01-05 15:20:45 +0800

    final version hw2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
 	@@all_ratings = ['G', 'PG', 'PG-13', 'R']
-	
+	#nifty addition to be used over in movies_controller for ratings
 	def self.all_ratings
 		@@all_ratings
 	end
